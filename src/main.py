0df0364c4207504ee8120c606beee872df2011b8
Administrator <228803287@qq.com>
2026-01-13 22:36:32 +0800

    tab

--- src/main.py
+++ src/main.py
@@ -17,23 +17,33 @@ def main(page: ft.Page):
     async def handle_show_drawer():
         await page.show_drawer()
 
-
-    page.appbar = ft.AppBar(
-    leading=ft.IconButton(ft.Icons.MENU, on_click=handle_show_drawer),
-    leading_width=40,
-    #title=ft.Text("AppBar Example"),
-    center_title=False,
-    bgcolor=ft.Colors.with_opacity(0.5, ft.Colors.BLUE),
-    actions_padding=10,
-    actions=[
-        ft.CircleAvatar(
-            content=ft.Text("AB"),
-            bgcolor=ft.Colors.PRIMARY,
-            color=ft.Colors.ON_PRIMARY,
-            max_radius=15
-        )
-    ],
+    appbar=ft.AppBar(
+        title=ft.Text("AppBar Example"),
+        leading=ft.IconButton(ft.Icons.MENU, on_click=handle_show_drawer),
+        leading_width=40,
+        #title=ft.Text("AppBar Example"),
+        center_title=False,
+        bgcolor=ft.Colors.with_opacity(0.0, ft.Colors.BLUE),
+        actions_padding=10,
+        actions=[
+            ft.CircleAvatar(
+                content=ft.Text("AB"),
+                bgcolor=ft.Colors.PRIMARY,
+                color=ft.Colors.ON_PRIMARY,
+                max_radius=15
+            )
+        ],
     )
+
+
+
+    page.appbar = appbar
+    def on_scroll(e: ft.OnScrollEvent):
+        print(e)
+        page.show_dialog(ft.SnackBar(ft.Text(f"scrolled{e.pixels,e.max_scroll_extent,e.min_scroll_extent}")))
+        appbar.title=f"scrolled{e.pixels,e.max_scroll_extent,e.min_scroll_extent}",
+        appbar.update()
+
     page.add(
         ft.ListView(
             expand=True,
@@ -43,7 +53,7 @@ def main(page: ft.Page):
                         ft.Column(expand=True,alignment=ft.MainAxisAlignment.START,controls=[ft.Container(height=60,bgcolor=ft.Colors.AMBER_200) for _ in range(55)]),
                         ],
             )],
-            on_scroll=lambda e: print(f"scrolled{e.pixels,e.max_scroll_extent,e.min_scroll_extent}"),
+            on_scroll=on_scroll
         )
         
     )
